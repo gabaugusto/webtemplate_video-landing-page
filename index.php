e0a7c6f4040b9c5d8df5fcc809e84761b86871da
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2020-11-16 11:09:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <title>Sample.com - buzzyo.xyz</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="description" content="A Ferramenta de negociação mais eficiente do mercado. " />
+    <meta http-equiv="keywords" content="excelência tecnica, acordo de dividas, experiência em negociacao, alta qualidade negociaco, negociao de baixo custo, discadores, cobranca em massa, cobranca, devedores em massa, produtos e servicos telecom" />
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1" />
+    <meta name="robots" content=" index,follow" />
+    <meta name="theme-color" content="#db5945">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+	<script type="text/javascript" src="assets/js/jquery.js"></script>
+</head>
+<body>
+<h2>Import CSV file into Mysql using PHP</h2>
+
+    <div id="response"
+        class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>">
+        <?php if(!empty($message)) { echo $message; } ?>
+        </div>
+    <div class="outer-scontainer">
+        <div class="row">
+
+            <form class="form-horizontal" action="" method="post"
+                name="frmCSVImport" id="frmCSVImport"
+                enctype="multipart/form-data">
+                <div class="input-row">
+                    <label class="col-md-4 control-label">Choose CSV
+                        File</label> <input type="file" name="file"
+                        id="file" accept=".csv">
+                    <button type="submit" id="submit" name="import"
+                        class="btn-submit">Import</button>
+                    <br />
+
+                </div>
+
+            </form>
+
+        </div>
+               <?php
+            $sqlSelect = "SELECT * FROM users";
+            $result = $db->select($sqlSelect);
+            if (! empty($result)) {
+                ?>
+            <table id='userTable'>
+            <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>User Name</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+
+                </tr>
+            </thead>
+<?php
+                
+                foreach ($result as $row) {
+                    ?>
+                    
+                <tbody>
+                <tr>
+                    <td><?php  echo $row['userId']; ?></td>
+                    <td><?php  echo $row['userName']; ?></td>
+                    <td><?php  echo $row['firstName']; ?></td>
+                    <td><?php  echo $row['lastName']; ?></td>
+                </tr>
+                    <?php
+                }
+                ?>
+                </tbody>
+        </table>
+        <?php } ?>
+    </div>
+
+    <script type="text/javascript">
+$(document).ready(function() {
+    $("#frmCSVImport").on("submit", function () {
+
+	    $("#response").attr("class", "");
+        $("#response").html("");
+        var fileType = ".csv";
+        var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + fileType + ")$");
+        if (!regex.test($("#file").val().toLowerCase())) {
+        	    $("#response").addClass("error");
+        	    $("#response").addClass("display-block");
+            $("#response").html("Invalid File. Upload : <b>" + fileType + "</b> Files.");
+            return false;
+        }
+        return true;
+    });
+});
+</script>
+
+</body>
+</html>
